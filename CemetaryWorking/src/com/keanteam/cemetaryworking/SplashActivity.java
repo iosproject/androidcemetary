a9aed2e1d1599f62f66ac69ebb65b7125333e65d
DJ <DJ@DJ-PC.hsd1.nj.comcast.net>
2012-09-30 00:26:13 -0400

    More Splash changes.  Splash to home is still buggy

--- CemetaryWorking/src/com/keanteam/cemetaryworking/SplashActivity.java
+++ CemetaryWorking/src/com/keanteam/cemetaryworking/SplashActivity.java
@@ -1,20 +1,34 @@
 package com.keanteam.cemetaryworking;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.view.animation.Animation;
 
-public class SplashActivity extends Activity {
+    public class SplashActivity extends Activity {
+    private static final int SPLASH_DISPLAY_TIME = 4000; // splash screen delay time
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+
+                Intent intent = new Intent();
+                intent.setClass(SplashActivity.this, HomeActivity.class);
+
+                SplashActivity.this.startActivity(intent);
+                SplashActivity.this.finish();
+
+                // transition from splash to main menu
+                overridePendingTransition(R.anim.homefadein,
+                        R.anim.splashfadeout);
+
+            }
+        }, SPLASH_DISPLAY_TIME);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_splash, menu);
-        return true;
-    }
 }
