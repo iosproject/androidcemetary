5e7d966c088398eeb89f89f3ce143679223edd07
DJ <DJ@DJ-PC.hsd1.nj.comcast.net>
2012-09-30 22:30:55 -0400

    More Changes and files added

--- CemetaryWorking/src/com/keanteam/cemetaryworking/MoreActivity.java
+++ CemetaryWorking/src/com/keanteam/cemetaryworking/MoreActivity.java
@@ -2,13 +2,14 @@ package com.keanteam.cemetaryworking;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.Menu;
 
-public class HomeActivity extends Activity {
+public class MoreActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home);
+        setContentView(R.layout.more);
     }
 
 }
